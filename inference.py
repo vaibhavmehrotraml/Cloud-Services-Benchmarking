e8c618a2cf1ae838953a00dec80d91a34a779251
Vaibhav Mehrotra <vaibhav1@uw.edu>
2023-12-04 05:50:09 -0600

    feat(.): Write base inference code

---
+++ inference.py
@@ -0,0 +1,46 @@
+import torch
+from efficientnet_pytorch import EfficientNet
+from torchvision import transforms
+from PIL import Image
+import os
+import time
+from tqdm import tqdm
+
+image_folder_path = 'data/dataset'
+
+
+model = EfficientNet.from_pretrained('efficientnet-b1')
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((240, 240)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+def predict_image(image_path):
+    image = Image.open(image_path).convert('RGB')
+    # Add batch dimension
+    image = transform(image).unsqueeze(0) 
+
+    with torch.no_grad():
+        outputs = model(image)
+    return torch.argmax(outputs, dim=1)
+
+start_time = time.time()
+
+predictions = []
+for image_name in tqdm(os.listdir(image_folder_path)):
+    if image_name.endswith('.csv'):
+        continue
+    image_path = os.path.join(image_folder_path, image_name)
+    prediction = predict_image(image_path)
+    predictions.append(prediction)
+
+end_time = time.time()
+total_time = end_time - start_time
+avg_time_per_image = total_time / len(predictions)
+
+print(f"Total inference time: {total_time} seconds")
+print(f"Average time per image: {avg_time_per_image} seconds")
